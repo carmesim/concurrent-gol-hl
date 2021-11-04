ffd0e1ae63658af6225a783a62fb49ac450e851a
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2021-11-04 06:05:45 -0300

    Atualiza resultados

--- Java/roda_testes.sh
+++ Java/roda_testes.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-./run_hl.sh $1 1  | grep -o "[0-9]*" | head -n1 > out.txt
-./run_hl.sh $1 2  | grep -o "[0-9]*" | head -n1 >> out.txt
-./run_hl.sh $1 4  | grep -o "[0-9]*" | head -n1 >> out.txt
-./run_hl.sh $1 8  | grep -o "[0-9]*" | head -n1 >> out.txt
+echo "======== GAME OF LIFE ========"
+./run_gol.sh 2048 1 > result_gol/saida1.txt
+cat result_gol/saida1.txt
+./run_gol.sh 2048 2 > result_gol/saida2.txt
+cat result_gol/saida2.txt
+./run_gol.sh 2048 4 > result_gol/saida4.txt
+cat result_gol/saida4.txt
+./run_gol.sh 2048 8 > result_gol/saida8.txt
+cat result_gol/saida8.txt
+echo "======== HIGH LIFE ========"
+./run_hl.sh 2048 1 > result_hl/saida1.txt
+cat result_hl/saida1.txt
+./run_hl.sh 2048 2 > result_hl/saida2.txt
+cat result_hl/saida2.txt
+./run_hl.sh 2048 4 > result_hl/saida4.txt
+cat result_hl/saida4.txt
+./run_hl.sh 2048 8 > result_hl/saida8.txt
+cat result_hl/saida8.txt
 
